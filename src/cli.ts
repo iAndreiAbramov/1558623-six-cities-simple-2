87535b8c8211364ab4a59685fd42cb7ce4c94029
Andrey_Abramov <i.andrei.abramov@gmail.com>
2022-09-10 16:17:09 +0300

    shebang added

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,5 @@
+#!/Users/user/.nvm/versions/node/v16.16.0/bin/node
+
 import CliApplication from './app/cli.application.js';
 import { helpCommand } from './cli-command/help.command.js';
 import { versionCommand } from './cli-command/version.command.js';
